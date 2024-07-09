98cab847d1fe66fe6835944dc0419cedd1e7a846
ngnamquoc <nguyennamquoc.mar@gmail.com>
2024-07-08 22:59:34 -0400

    draft page layout with header, footer and the home page (without styling)

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ const port=4000 || process.env.PORT;
 //set the static directory
 app.use(express.static("public"));
 
-//Set template
+//Set template engine
 app.use(expressLayout);
-app.set('layout','./layouts/main');
 app.set('view engine','ejs');
+
+//set the main layout path
+app.set('layout','./layout/main');
 
 //routes
 app.get("/",(req,res)=>{
-    res.send("Hello");
+    const locals={
+        "title":"Node Js Blog",
+        "description":"This is a simple blog created with NodeJs, Express and Postgre.",
+    }
+    res.render('index',{locals});
+});
+app.get("/about",(req,res)=>{
+    res.render('about');
 });
 
 app.listen(port, ()=>{
--- server/routes/main.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-//Routes
-router.get("/",(req,res)=>{
-    res.send("hello");
-})
-
-module.exports = router;
